146f88e0a4bca19aa81c0805f2bc1eff77e83b20
JP-Pardinho <jppardinhor@gmail.com>
2025-08-03 23:52:17 -0300

    Docs: Estrutura do arquivos

---
+++ ED1_Grupo1_JPPardinho_Nicolas_CarlosEduardo_Joaquim/Pilha.h
@@ -0,0 +1,16 @@
+/*
+    Trabalho Estrutura de dados I / 2025-1 
+    GRUPO 1: João Pedro Pardinho, Nícolas Leal, Joaquim Moizes, Carlos Eduardo de Oliveira
+*/
+
+#ifndef __PILHA_H__
+#define __PILHA_H__
+
+typedef struct Pilha
+{
+    /* preencher */
+}P;
+
+/* declarar funções genericas */
+
+#endif
---
+++ ED1_Grupo1_JPPardinho_Nicolas_CarlosEduardo_Joaquim/programa1.c
@@ -0,0 +1,4 @@
+/*
+    Trabalho Estrutura de dados I / 2025-1 
+    GRUPO 1: João Pedro Pardinho, Nícolas Leal, Joaquim Moizes, Carlos Eduardo de Oliveira
+*/
