1975964a8819f428044c48e889389d769c19031a
JP-Pardinho <jppardinhor@gmail.com>
2025-08-06 15:24:06 -0300

    feat: upando as alterações

--- ED1_Grupo1_JPPardinho_Nicolas_CarlosEduardo_Joaquim/Pilha.h
+++ ED1_Grupo1_JPPardinho_Nicolas_CarlosEduardo_Joaquim/Pilha.h
@@ -8,6 +8,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 typedef void* Generico;
 typedef struct no {
--- ED1_Grupo1_JPPardinho_Nicolas_CarlosEduardo_Joaquim/programa1.c
+++ ED1_Grupo1_JPPardinho_Nicolas_CarlosEduardo_Joaquim/programa1.c
@@ -12,6 +12,88 @@
 
 #include "Pilha.h"
 
+// Função auxiliar para verificar se um caractere é um delimitador de abertura
+int ehAbertura(char c)
+{
+    if ( c == '(' || c == '[' || c == '{') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// Função auxiliar para verificar se um caractere é um delimitador de fechamento
+int ehFechamento(char c)
+{
+    if (c == ')' || c == ']' || c == '}') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// Função auxiliar para verificar se os delimitadores correspondem
+int corresponde(char abertura, char fechamento)
+{
+    return (abertura == '(' && fechamento == ')') ||
+           (abertura == '[' && fechamento == ']') ||
+           (abertura == '{' && fechamento == '}');
+}
+
+// Problema 1 a) - Verifica apenas o fechamento correto dos delimitadores
+int verificaFechamento(char *expressao)
+{
+    Pilha *p = criaPilha();
+    int resultado = 1; // Assume que é válido inicialmente
+
+    for (int i = 0; expressao[i] != '\0'; i++)
+    {
+        char c = expressao[i];
+
+        if (ehAbertura(c))
+        {
+            empilhar(p, (Generico)(size_t)c);
+        }
+        else if (ehFechamento(c))
+        {
+            if (verificaPilhaVazia(p))
+            {
+                resultado = 0;
+                break;
+            }
+
+            char topo = (char)(size_t)desempilhar(p);
+            if (!corresponde(topo, c))
+            {
+                resultado = 0;
+                break;
+            }
+        }
+    }
+
+    // Se a pilha não estiver vazia no final, faltaram fechamentos
+    if (!verificaPilhaVazia(p))
+    {
+        resultado = 0;
+    }
+
+    destroiPilha(&p);
+    return resultado;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Problema 1 a)
 int verificaFechamento(Pilha p){
     // CADA TAG DE ABERTURA TEM QUE ESTÁ FECAHDO CORRETAMENTE
